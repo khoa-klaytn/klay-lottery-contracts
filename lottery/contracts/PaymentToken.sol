bb71edf927da21ac26b3cc6a84acecaae41996c3
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-02 13:49:23 +0800

    refactor(PaymentToken.demand): show value & amount in error message

--- lottery/contracts/PaymentToken.sol
+++ lottery/contracts/PaymentToken.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.16;
 
 import {IPaymentToken} from "./interfaces/IPaymentToken.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PaymentToken is IPaymentToken {
     function send(address recipient, uint256 amount) external override returns (bool sent) {
@@ -9,7 +10,7 @@ contract PaymentToken is IPaymentToken {
     }
 
     function demand(uint256 amount) external payable override {
-        require(msg.value >= amount, "Not enough KLAY");
+        require(msg.value >= amount, string.concat(Strings.toString(msg.value), " < ", Strings.toString(amount)));
     }
 
     function getBalance(address account) external view override returns (uint256) {
