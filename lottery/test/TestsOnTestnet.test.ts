f94aba18a4997c5f965eee107faa37d9ec681043
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-03 11:23:41 +0800

    refactor(tests): combine sendTransaction & waitResponse into sendFn w/ optional waiting

--- lottery/test/TestsOnTestnet.test.ts
+++ lottery/test/TestsOnTestnet.test.ts
@@ -52,30 +52,6 @@ async function deployContract(contractName: ContractName, args: any[] = []) {
 }
 
 /**
- * Create a signer & use it to call a contract function
- * @param args Refer to contract ABI
- * @returns Contract function response
- */
-async function sendTransaction(
-  accountName: AccountName,
-  contractName: ContractName,
-  functionName: string,
-  args: any[],
-  overrides?: ethers.Overrides
-) {
-  const signer = contracts[contractName].contract.connect(wallets[accountName]);
-  const fn = signer[functionName];
-  let response: ethers.TransactionResponse;
-  if (overrides) {
-    console.log("Overrides: ", overrides);
-    response = await fn(...args, overrides);
-  } else {
-    response = await fn(...args);
-  }
-  console.log(`Transaction sent: ${response.hash}`);
-  return response as ethers.TransactionResponse;
-}
-/**
  * Wait for a transaction to be mined, handle transaction replacement
  * @param _response Response to wait for
  * @returns (replaced) response & receipt
@@ -84,7 +60,7 @@ async function waitResponse(_response: ethers.TransactionResponse) {
   let response = _response;
   let receipt: ethers.TransactionReceipt;
   try {
-    const _receipt = await _response.wait(1);
+    const _receipt = await response.wait(1);
     if (!_receipt) {
       throw new Error("Receipt not found");
     }
@@ -103,6 +79,46 @@ async function waitResponse(_response: ethers.TransactionResponse) {
     throw e;
   }
   return [response, receipt] as const;
+}
+
+/**
+ * Create a signer & use it to call a contract function
+ * @returns Contract function response
+ */
+async function _sendFn(
+  accountName: AccountName,
+  contractName: ContractName,
+  functionName: string,
+  args?: any[],
+  overrides?: ethers.Overrides
+) {
+  if (!args) args = [];
+
+  const signer = contracts[contractName].contract.connect(wallets[accountName]);
+  const fn = signer[functionName];
+  let response: ethers.TransactionResponse;
+  if (overrides) {
+    console.log("Overrides: ", overrides);
+    response = await fn(...args, overrides);
+  } else {
+    response = await fn(...args);
+  }
+  console.log(`Transaction sent: ${response.hash}`);
+  return response;
+}
+
+type _SendFnP = Parameters<typeof _sendFn>;
+type _SendFnR = ReturnType<typeof _sendFn>;
+type WaitResponseR = ReturnType<typeof waitResponse>;
+
+function sendFn(a: _SendFnP, wait?: true): WaitResponseR;
+function sendFn(a: _SendFnP, wait: false): _SendFnR;
+async function sendFn(a: _SendFnP, wait = true) {
+  const response = await _sendFn(...a);
+  if (wait) {
+    return waitResponse(response);
+  }
+  return response;
 }
 
 function sleep(duration: number) {
@@ -138,22 +154,6 @@ describe("Lottery on Testnet", () => {
     }
   });
 
-  async function setLotteryAddress() {
-    const setLotteryAddressResponse = await sendTransaction("alice", "RandomNumberGenerator", "setLotteryAddress", [
-      contractsConfig.KlayLottery.address,
-    ]);
-    await setLotteryAddressResponse.wait(1);
-  }
-  async function setRoles() {
-    const setRolesResponse = await sendTransaction(
-      "alice",
-      "KlayLottery",
-      "setOperatorAndTreasuryAndInjectorAddresses",
-      [wallets.operator.address, wallets.treasury.address, wallets.injector.address]
-    );
-    await setRolesResponse.wait(1);
-  }
-
   before(async () => {
     if (!contractsConfig.RandomNumberGenerator.address)
       contractsConfig.RandomNumberGenerator.address = await deployContract("RandomNumberGenerator", [
@@ -168,8 +168,13 @@ describe("Lottery on Testnet", () => {
 
     await Promise.all(contractPromises);
 
-    await setLotteryAddress();
-    await setRoles();
+    await sendFn(["alice", "RandomNumberGenerator", "setLotteryAddress", [contractsConfig.KlayLottery.address]]);
+    await sendFn([
+      "alice",
+      "KlayLottery",
+      "setOperatorAndTreasuryAndInjectorAddresses",
+      [wallets.operator.address, wallets.treasury.address, wallets.injector.address],
+    ]);
   });
 
   // ---- //
@@ -180,14 +185,13 @@ describe("Lottery on Testnet", () => {
 
     it("Operator starts lottery", async () => {
       endTime = BigInt(await time.latest()) + _lengthLottery;
-      const startResponse = await sendTransaction("operator", "KlayLottery", "startLottery", [
-        endTime.toString(),
-        _priceTicket.toString(),
-        _discountDivisor,
-        _rewardsBreakdown,
-        _treasuryFee,
+      const startTx = await sendFn([
+        "operator",
+        "KlayLottery",
+        "startLottery",
+        [endTime.toString(), _priceTicket.toString(), _discountDivisor, _rewardsBreakdown, _treasuryFee],
       ]);
-      const startReceipt = (await waitResponse(startResponse))[1];
+      const startReceipt = startTx[1];
       let lotteryOpenLog: ethers.LogDescription;
       const lotteryOpenEvent = contracts.KlayLottery.abi.getEventName("LotteryOpen");
       for (const log of startReceipt.logs) {
@@ -210,24 +214,33 @@ describe("Lottery on Testnet", () => {
     });
 
     it("Bob buys 1 ticket", async () => {
-      const buyTicketsResponse = await sendTransaction("bob", "KlayLottery", "buyTickets", [lotteryId, ["1234561"]], {
-        value: _priceTicket,
-      });
-      await waitResponse(buyTicketsResponse);
+      await sendFn([
+        "bob",
+        "KlayLottery",
+        "buyTickets",
+        [lotteryId, ["1234561"]],
+        {
+          value: _priceTicket,
+        },
+      ]);
     });
 
     it("Injector injects funds", async () => {
-      const injectFundsResponse = await sendTransaction("injector", "KlayLottery", "injectFunds", [lotteryId], {
-        value: ethers.parseEther("0.5"),
-      });
-      await waitResponse(injectFundsResponse);
+      await sendFn([
+        "injector",
+        "KlayLottery",
+        "injectFunds",
+        [lotteryId],
+        {
+          value: ethers.parseEther("0.5"),
+        },
+      ]);
     });
 
     it("Operator closes lottery", async () => {
       // Wait for lottery to end
       await sleep(Number(_lengthLottery) * 1000);
-      const closeResponse = await sendTransaction("operator", "KlayLottery", "closeLottery", [lotteryId]);
-      await waitResponse(closeResponse);
+      await sendFn(["operator", "KlayLottery", "closeLottery", [lotteryId]]);
     });
   });
 });
