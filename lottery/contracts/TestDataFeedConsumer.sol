f8bcc7dcc6b9ee57d83f8d90097982df816ba3fe
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-23 19:22:14 +0800

    refactor(DataFeedConsumer): dont set state on getLatestData

--- lottery/contracts/TestDataFeedConsumer.sol
+++ lottery/contracts/TestDataFeedConsumer.sol
@@ -6,13 +6,9 @@ import "./DataFeedConsumer.sol";
 contract TestDataFeedConsumer is DataFeedConsumer {
     constructor(address aggregatorProxy) DataFeedConsumer(aggregatorProxy) {}
 
-    function getLatestDataTest() public returns (int256, uint8) {
+    function getLatestDataTest() external view returns (int256, uint8) {
         (, int256 answer_, , , ) = dataFeed.latestRoundData();
         uint8 decimals_ = dataFeed.decimals();
-
-        answer = answer_;
-        decimals = decimals_;
-
         return (answer_, decimals_);
     }
 }
--- lottery/contracts/interfaces/IDataFeedConsumer.sol
+++ lottery/contracts/interfaces/IDataFeedConsumer.sol
@@ -4,9 +4,5 @@ pragma solidity ^0.8.16;
 import {IAggregator} from "@bisonai/orakl-contracts/src/v0.1/interfaces/IAggregator.sol";
 
 interface IDataFeedConsumer {
-    function answer() external view returns (int256);
-
-    function decimals() external view returns (uint8);
-
     function getLatestData() external view returns (int256, uint8);
 }
