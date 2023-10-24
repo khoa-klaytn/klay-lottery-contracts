6665a020003f8750e0f349a286865a82428e4000
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-24 11:03:32 +0800

    feat(TestDataFeedConsumer): add getLatestDataTest

--- lottery/contracts/TestDataFeedConsumer.sol
+++ lottery/contracts/TestDataFeedConsumer.sol
@@ -6,6 +6,10 @@ import "./DataFeedConsumer.sol";
 contract TestDataFeedConsumer is DataFeedConsumer {
     constructor(address aggregatorProxy) DataFeedConsumer(aggregatorProxy) {}
 
+    function getLatestDataTest() external view returns (uint256) {
+        return _getLatestData();
+    }
+
     function convertCryptoUsdTest(uint256 crypto) external view returns (uint256 usd) {
         return _convertCryptoUsd(crypto);
     }
